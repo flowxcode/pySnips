a9916bd69b842e6ebf2784fd113f0f6ae75b353a
flo isopp <florian.johannes.isopp@gmail.com>
2020-11-21 17:32:47 +0100

    z3 garden solver

--- z3v19.py
+++ z3v19.py
@@ -2,11 +2,39 @@
  
 # z3-solver games
 
+import statistics
 from z3 import *
- 
+
+
+
+
 def main():
+
+    arr = [tuple([i]*5) for i in range(5)]
+
+
+
+    table = [ BitVecs('go_%d0 c_%d1 c_%d2 c_%d3 c_%d4' % tuple([i]*5), 32) for i in range(5) ]
+    print(table)
+    
+    print(table[0])
+    print(table[0][0])
+
+
+    for r in table:
+        for c in r:
+            print(c,end = " ")
+        print()
+  
+    # unsorted list of random integers 
+    data1 = [2, -2, 3, 6, 9, 4, 5, -1] 
+    print("Median of data-set is : % s " % (statistics.median(data1)))
+
+    data1 = [234, -2, 3, 62, 9, 4, 5, -1, 1, 1, 1, 1, 1, 1, 1, 1]
+    print("Median of data-set is : % s " % (statistics.median(data1)))
 	
     person = list()
+
 
     # TODO modify for writing database as csv
     fieldnames = ["Age", "Male", "Salary (k)"]
@@ -14,10 +42,29 @@ def main():
     s = Solver()
     age = IntVector('age', 10)
     male = BoolVector('male', 10)
-    salary = IntVector('salary', 10)
-    
-    # Printing the mean 
-    print("Mean is :", x)
+    salary = IntVector('salary', 10)    
+
+    s.add([18 <= age[i] for i in range(10)])
+    s.add([100 >= age[i] for i in range(10)])
+    s.add([20 <= salary[i] for i in range(10)])
+    s.add([100 >= salary[i] for i in range(10)])
+
+    print(Sum([age[i] for i in range(10)]) / (4) == 58)
+    print(Sum([If(And(male[i] == True, True), age[i], 0) for i in range(10)]) / (4) == 59)
+
+    print([age[i] for i in range(10)])
+    # print(medianx([age[i] for i in range(10)]))
+    # if statistics.median([age[i] for i in range(10)]) == 1:
+    #     print(1)
+
+    print(sorted([0,3,2]))
+
+
+    s.add(Sum([If(And(male[i] == True, True), age[i], 0) for i in range(10)]) / (4) == 59)
+    s.add(Sum(sorted( [age[i] for i in range(10)] ))==1)
+
+    s.add(Sum(sorted([age[i] for i in range(10)]))/2 == 1)
+    s.add(medianx([age[i] for i in range(10)]) == 69)
 
     for i in range(10):
         if s.check() == sat:
