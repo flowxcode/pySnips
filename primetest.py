abbbe294d21fee48a52d513bb77bb73b9e5a6001
flowxcode <florian.johannes.isopp@gmail.com>
2020-11-24 00:19:25 +0100

    prime basic fkt, mock fkt

--- primetest.py
+++ primetest.py
@@ -9,15 +9,26 @@ import string
 from models.BColors import BColors
 
 
-def isprime(x):
+# framework
 
+def mock():
+    while i ** 2 <= n:
+        print("i:" + str(i) + ", n:" + str(n))
+        if n % i == 0 or n % (i + 2) == 0:
+            print("break")
+            break
+        i += 6    
+    print("the end")
+
+
+def isprime(x):
     for i in range(2, x-1):
         if x % i == 0:
             print(BColors.WARNING + "oooohh noooooooooo" + BColors.ENDC)
             return False
-    else:
-        print(BColors.OKCYAN + "Christmas got a present for you. Its a prime number" + BColors.ENDC)
-        return True
+        else:
+            print(BColors.OKCYAN + "Christmas got a present for you. Its a prime number" + BColors.ENDC)
+            return True
 
 
 def main():
@@ -37,7 +48,7 @@ def main():
 
     if args.command is None:
         print("noooo arg......... testing 11")
-        if isprime(11):
+        if isprime(56243542345434):
             return 0
         return 1
 
